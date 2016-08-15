1811a2049222eadc042899149387bbd0ff787cf8
Matthew R Hanlon <mrhanlon@gmail.com>
2016-08-14 21:21:30 -0500

    unused routes

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ var bodyParser = require('body-parser');
 var hbs = require('hbs');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
-
 
 var app = express();
 
@@ -27,7 +25,6 @@ app.use(require('node-compass')({mode: 'expanded'}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
